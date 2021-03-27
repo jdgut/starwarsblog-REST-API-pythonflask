9906f79dacde361b0562edadd12843d8fa43734e
jdgut <jdgutierrez80@gmail.com>
2021-03-27 01:39:18 +0000

    adding initial py settings for Flask API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -20,6 +20,48 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+
+@app.route('/users')
+def handle_users():
+    users = list(map(lambda user: user.serialize(), User.query.filter_by(is_active=1).all())) 
+    return jsonify(users), 200
+
+@app.route('/users/<int:user_id>/favorites', methods=['GET', 'POST'])
+def handle_user_favorites(user_id):
+    # get only the ones named "Joe"
+    favorites_query = Favorite.query.filter_by(user_id=user_id).all()
+
+    if len(favorites_query) == 0:
+        return jsonify({'error' : f'No favorites assigned to user_id {user_id}'}), 404
+
+    # map the results and your list of people  inside of the all_people variable
+    favorites = list(map(lambda favorite: favorite.serialize(), favorites_query))
+
+    return jsonify(favorites), 200
+
+
+@app.route('/people')
+def handle_people():
+    people = list(map(lambda character: character.serialize(), People.query.all()))
+    return jsonify(people), 200
+
+@app.route('/people/<int:people_id>')
+def handle_people_details(people_id):
+    return 'people details'
+
+@app.route('/planets')
+def handle_planets():
+    planets = list(map(lambda planet: planet.serialize(), Planet.query.all()))
+    return jsonify(planets), 200
+
+@app.route('/planets/<int:planet_id>')
+def handle_planet_details(planet_id):
+    return 'planet_details'
+
+@app.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+def handle_delete_favorites(favorite_id):
+    return 'delete_favorites'
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +72,14 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# @app.route('/user', methods=['GET'])
+# def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+#     response_body = {
+#         "msg": "Hello, this is your GET /user response "
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    name= db.Column(db.String(250), nullable=False)
+    location= db.Column(db.String(250), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +14,79 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "location": self.location,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer(), nullable=False)
+    mass = db.Column(db.Integer(), nullable=False)
+    hair_color = db.Column(db.String(120), nullable=False)
+    skin_color = db.Column(db.String(120), nullable=False)
+    eye_color  = db.Column(db.String(120), nullable=False)
+    gender  = db.Column(db.String(120), nullable=False)
+    birth_year  = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "height" : self.height,
+            "mass" : self.mass,
+            "hair_color" : self.hair_color,
+            "skin_color" : self.skin_color, 
+            "eye_color"  : self.eye_color,
+            "gender" : self.gender,
+            "birth_year"  : self.birth_year
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    rotation_period = db.Column(db.Integer(), nullable=False)
+    orbital_period = db.Column(db.Integer(), nullable=False)
+    diameter = db.Column(db.Integer(), nullable=False)
+    climate = db.Column(db.String(120), nullable=False)
+    terrain  = db.Column(db.String(120), nullable=False)
+    population  = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "rotation_period" : self.rotation_period,
+            "orbital_period" : self.orbital_period,
+            "diameter" : self.diameter,
+            "climate" : self.climate, 
+            "terrain"  : self.terrain,
+            "population" : self.population
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, default=0)
+    planet_id = db.Column(db.Integer, default=0)
+    people_id = db.Column(db.Integer, default=0)
+
+
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "user_id" : self.user_id,
+            "planet_id" : self.planet_id,
+            "people_id" : self.people_id
         }
