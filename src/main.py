8d939a15e58f4ab9e2f6eef5d3a3646aa7d0328d
jdgut <jdgutierrez80@gmail.com>
2021-03-27 01:54:40 +0000

    latest version

--- src/main.py
+++ src/main.py
@@ -29,16 +29,15 @@ def handle_users():
 @app.route('/users/<int:user_id>/favorites', methods=['GET', 'POST'])
 def handle_user_favorites(user_id):
     # get only the ones named "Joe"
-    favorites_query = Favorite.query.filter_by(user_id=user_id).all()
+    favorites = list(map(lambda favorite: favorite.serialize(), Favorite.query.filter_by(user_id=user_id).all()))
 
-    if len(favorites_query) == 0:
-        return jsonify({'error' : f'No favorites assigned to user_id {user_id}'}), 404
+    if not favorites:
+       raise APIException('No favorites assigned', status_code=404)
 
     # map the results and your list of people  inside of the all_people variable
     favorites = list(map(lambda favorite: favorite.serialize(), favorites_query))
 
     return jsonify(favorites), 200
-
 
 @app.route('/people')
 def handle_people():
@@ -47,7 +46,12 @@ def handle_people():
 
 @app.route('/people/<int:people_id>')
 def handle_people_details(people_id):
-    return 'people details'
+    #character = People.query.filter_by(id=people_id).first_or_404()
+    character = People.query.filter_by(id=people_id).first()
+    if not character:
+         raise APIException('Character not found', status_code=404)
+
+    return jsonify(character.serialize()), 200
 
 @app.route('/planets')
 def handle_planets():
@@ -56,11 +60,25 @@ def handle_planets():
 
 @app.route('/planets/<int:planet_id>')
 def handle_planet_details(planet_id):
-    return 'planet_details'
+    planet = Planet.query.filter_by(id=planet_id).first()
+    if not planet:
+        raise APIException('Planet not found', status_code=404)
+
+    return jsonify(planet.serialize()), 200
 
 @app.route('/favorite/<int:favorite_id>', methods=['DELETE'])
 def handle_delete_favorites(favorite_id):
-    return 'delete_favorites'
+    favorite = Favorite.query.filter_by(id=favorite_id).first()
+
+    if not favorite:
+        raise APIException('Favorite not found', status_code=404)
+
+    db.session.delete(favorite)
+    db.session.commit()
+
+    return jsonify(''), 204
+    
+   
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -71,15 +89,6 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
-# @app.route('/user', methods=['GET'])
-# def handle_hello():
-
-#     response_body = {
-#         "msg": "Hello, this is your GET /user response "
-#     }
-
-#     return jsonify(response_body), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
